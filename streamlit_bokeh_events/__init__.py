4cc3913acced5b92b396914bf1d18bc97d3ff096
Ashish Shukla <ashish.shukla@hitachiconsulting.com>
2020-07-25 17:49:11 +0530

    added new demo video with debounce time feature

--- streamlit_bokeh_events/__init__.py
+++ streamlit_bokeh_events/__init__.py
@@ -6,7 +6,7 @@ from random import choices
 from string import ascii_letters
 import streamlit.components.v1 as components
 
-_RELEASE = False
+_RELEASE = True
 
 if not _RELEASE:
     _component_func = components.declare_component(
@@ -35,17 +35,19 @@ def streamlit_bokeh_events(bokeh_plot=None, events="", key=None, debounce_time=1
 
 if not _RELEASE:
     import streamlit as st
-    from random import random
     from bokeh.models import ColumnDataSource, CustomJS
     from bokeh.plotting import figure
+    import pandas as pd
+    import numpy as np
 
-    x = [random() for x in range(500)]
-    y = [random() for y in range(500)]
-    size = [random() * 10 for _ in range(500)]
+    df = pd.DataFrame({"x": np.random.rand(500), "y": np.random.rand(500), "size": np.random.rand(500) * 10})
 
-    source = ColumnDataSource(data=dict(x=x, y=y, size=size))
-    plot = figure(sizing_mode="scale_both", tools="lasso_select", title="Select Here")
-    plot.circle("x", "y", size="size", source=source, alpha=0.6)
+    source = ColumnDataSource(df)
+    
+    st.subheader("Select Points From Map")
+
+    plot = figure( tools="lasso_select", width=250, height=250)
+    plot.circle(x="x", y="y", size="size", source=source, alpha=0.6)
 
     source.selected.js_on_change(
         "indices",
@@ -66,9 +68,13 @@ if not _RELEASE:
         debounce_time=1000,
     )
 
-    x = [random() for x in range(500)]
-    y = [random() for y in range(500)]
-    size = [random() * 10 for _ in range(500)]
-    source.data = {"x": x, "y": y, "size": size}
+    # some event was thrown
+    if event_result is not None:
+        # TestSelectEvent was thrown
+        if "TestSelectEvent" in event_result:
+            st.subheader("Selected Points' Pandas Stat summary")
+            indices = event_result["TestSelectEvent"].get("indices", [])
+            st.table(df.iloc[indices].describe())
 
+    st.subheader("Raw Event Data")
     st.write(event_result)
